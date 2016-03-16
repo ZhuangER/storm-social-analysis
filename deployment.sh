823fa9699f911be5ce185532a7cc02e2e4100931
yu huang <286417333@qq.com>
2016-03-15 23:14:18 -0400

    renew storm version

--- deployment.sh
+++ deployment.sh
@@ -9,6 +9,7 @@
 # 3) -o pipefail means that if a step in a pipe fails, the whole pipe fails, which in combination with 1) means
 #    that the script as a whole fails.
 
+#set -euo pipefail
 
 sudo apt-get update -y
 
@@ -21,8 +22,8 @@ echo "Storm..."
 # TODO maybe make this use the best mirror always?
 sudo mkdir /opt/storm
 cd /opt/storm
-sudo wget http://mirror.cogentco.com/pub/apache/incubator/storm/apache-storm-0.9.2-incubating/apache-storm-0.9.2-incubating.tar.gz
-sudo tar xvzf apache-storm-0.9.2-incubating.tar.gz
-sudo rm apache-storm-0.9.2-incubating.tar.gz
-sudo chmod +x /opt/storm/apache-storm-0.9.2-incubating/bin/storm
-sudo ln -s /opt/storm/apache-storm-0.9.2-incubating/bin/storm /usr/bin/storm
+sudo wget http://apache.mirror.gtcomm.net/storm/apache-storm-0.9.6/apache-storm-0.9.6.tar.gz
+sudo tar xvzf apache-storm-0.9.6-src.tar.gz
+sudo rm apache-storm-0.9.6-src.tar.gz
+sudo chmod +x /opt/storm/apache-storm-0.9.6/bin/storm
+sudo ln -s /opt/storm/apache-storm-0.9.6/bin/storm /usr/bin/storm
